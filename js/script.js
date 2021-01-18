3bab2bbcc9b8d62d67f23f4a087f7d2cc94dd2bd
1000J_Iyn <iskim328@gmail.com>
2021-01-18 16:50:36 +0900

    Done

--- js/script.js
+++ js/script.js
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       
       // var homeHtmlToInsertIntoMainPage = ....
 
-      chosenCategoryShortName = "'" + chosenCategoryShortName = "'";
+      chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
       car homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
